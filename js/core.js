3254fd67b1889b5b398e6786388d49d102660058
Amir HTH <uo240753@uniovi.es>
2018-11-22 17:45:02 +0100

    a

--- js/core.js
+++ js/core.js
@@ -5,10 +5,13 @@ var logPartida="";
 var horaInicio="";
 var horaFin="";
 var imagenes = new Array() ;
+var erroresIMG = new Array() ;
+var aciertosIMG = new Array() ;
 var banderasSeleccionadas = new Array();
 var aciertos=0;
 var fallos=0;
 var tiempoEtapasCSV="";
+
 
 var elegido="";
 var turno=0;
@@ -22,6 +25,7 @@ function log2csv () {
     let separador=",";
     var csv=horaInicio+separador+horaFin+separador+idUSR+separador+generoUSR+separador+edadUSR+separador+aciertos+separador+fallos+separador+tiempoEtapasCSV
     console.log(csv) 
+    return csv;
 }
 
 function login() {
@@ -60,15 +64,39 @@ function initPartida() {
         console.log("click en: "+event.target.id);
         var urlIMG = document.getElementById(event.target.id).src;
         console.log("url bandera: "+urlIMG);
+        muestraResultado();
+
+        loguea("url bandera que salio: "+urlIMG+" bandera buena: "+elegido +" banderas: "+ banderasSeleccionadas.join(separador));
 
         if (urlIMG.includes(banderasSeleccionadas[elegido])) {
             aciertos++;
             document.getElementById('aciertos').innerHTML = "Aciertos: "+aciertos;
+           // imgResultado
+           
+
+            var aciertoLargo = aciertosIMG.length;
+            var numAciertoNueva = Math.round(Math.random()*(aciertoLargo-1));
+            console.log(numAciertoNueva);
+            document.getElementById("imgResultado").src="imgs/"+aciertosIMG[numAciertoNueva];
+            document.getElementById('txtResultado').innerHTML = "CORRECTO :) ";
+            document.getElementById('txtResultadoHeader').innerHTML = "CORRECTO :) ";
+            document.getElementById("myModal2").style.color = "green";
+            document.getElementById("myModal2").style.backgroundColor   = "DarkGreen";
+            document.getElementById("idResultadoHeader").style.backgroundColor  = "green";    
 
 
         } else {
             fallos++;
             document.getElementById('errores').innerHTML = "Errores: "+fallos;
+            var erroresLargo = erroresIMG.length;
+            var numErrorNueva = Math.round(Math.random()*(erroresLargo-1));
+            console.log(numErrorNueva);
+            document.getElementById("imgResultado").src="imgs/"+erroresIMG[numErrorNueva];
+            document.getElementById('txtResultado').innerHTML = "ERROR :( ";
+            document.getElementById('txtResultadoHeader').innerHTML = "ERROR :( ";
+            document.getElementById("myModal2").style.color = "red";
+            document.getElementById("myModal2").style.backgroundColor  = "DarkRed";
+            document.getElementById("idResultadoHeader").style.backgroundColor  = "green";
 
 
         }
@@ -84,6 +112,31 @@ function initPartida() {
              +" ha finalizado la partida a las "+ horaFin);
              console.log(logPartida);
              log2csv();
+            var templateParams = {
+                name: 'Resultado banderas',
+                "reply_to": "amazin.travels.miw@gmail.com",
+                "from_name": "MIW",
+                "to_name": "MIW",
+                notes: logPartida + " @@@_@@@ " + log2csv ()
+            };
+             
+            emailjs.send('gmail', 'template_MgjD9QFt', templateParams)
+                .then(function(response) {
+                   console.log('SUCCESS!', response.status, response.text);
+                }, function(error) {
+                   console.log('FAILED...', error);
+                });
+
+                var template_params = {
+                    "reply_to": "amazin.travels.miw@gmail.com",
+                    "from_name": "MIW",
+                    "to_name": "Juego banderas Log",
+                    "message_html": "message_html_value"
+                 }
+                 
+                 var service_id = "default_service";
+                 var template_id = "template_MgjD9QFt";
+                 emailjs.send(service_id,template_id,template_params);
 
             alert("Gracias por participar y por favor recuerde rellenar la encuesta");
         }
@@ -145,6 +198,29 @@ function initPartida() {
     imagenes[18] = "wisconsin.png"
     imagenes[19] = "wyoming.png"
 
+    aciertosIMG[0]="ok1.gif";
+    aciertosIMG[1]="ok2.jpg";
+    aciertosIMG[2]="ok3.png";
+    aciertosIMG[3]="ok4.jpg";
+    aciertosIMG[4]="cooperSonrie.png";
+    aciertosIMG[5]="ok.jpg";
+    aciertosIMG[6]="ok6.jpg";
+
+
+    erroresIMG[0]="error.png";
+    erroresIMG[1]="error_2.png";
+    erroresIMG[2]="error_3.png";
+    erroresIMG[3]="error_4.png";
+    erroresIMG[4]="cooper_miradas.jpg";
+    erroresIMG[5]="error_5.png";
+    erroresIMG[6]="error_6.gif";
+    erroresIMG[7]="error_6.gif";
+    erroresIMG[8]="error1.jpg";
+    erroresIMG[9]="error.png";
+    erroresIMG[9]="bazinga.jpg";
+
+
+
     initRonda();
 }
 
@@ -192,8 +268,8 @@ document.getElementById("e4_pic").src="banderas/"+imagenes[numImg4];
 
 banderasSeleccionadas[0]=imagenes[numImg1];
 banderasSeleccionadas[1]=imagenes[numImg2];
-banderasSeleccionadas[2]=imagenes[numImg3]
-banderasSeleccionadas[3]=imagenes[numImg4]
+banderasSeleccionadas[2]=imagenes[numImg3];
+banderasSeleccionadas[3]=imagenes[numImg4];
 
 
 
@@ -253,3 +329,30 @@ function reiniciar () {
 
 }
 
+
+
+// When the user clicks the button, open the modal 
+function muestraResultado() {
+
+    modal.style.display = "block";
+    setTimeout(function() {
+        modal.style.display = "none";
+      }, 3000);
+      
+      
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+
+
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
